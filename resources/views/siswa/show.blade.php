c525270cd8cc15f933cfa27965546f97101914ae
User <151610219@smkassalaambandung.sch.id>
2017-08-02 08:18:30 +0700

    crudanak

--- resources/views/siswa/show.blade.php
+++ resources/views/siswa/show.blade.php
@@ -2,11 +2,12 @@
 @section('isi')
 <div class="row">
 	<center><h1>Data Siswa</h1></center>
-	<div class="panel-panel-primary">
-		<div class="panel-headling">Data Siswa
+	<div class="panel panel-primary">
+		<div class="panel-heading">Data Siswa
 		<div class="panel-title pull-right">
 		<a href="{{URL::previous() }}">Kembali</a></div>
 		</div>
+
 		<div class="panel-body">
 			<form >
 				<div class="form-group">
@@ -15,7 +16,7 @@
 				</div>
 				<div class="form-group">
 					<label class="control-lable">Nama Orangtua</label>
-					<input type="text" class="form-control" name="b" value="{{$anak->orangtua->nama_ayah}} dan {{$siswa->orangtua->nama_ibu}}" readonly="">
+					<input type="text" class="form-control" name="b" value="{{$siswa->orangtua->nama_ayah}} dan {{$siswa->orangtua->nama_ibu}}" readonly="">
 					</input>
 				</div>
 				<div class="form-group">
@@ -28,7 +29,7 @@
 				</div>
 				<div class="form-group">
 					<label class="control-lable">Alamat</label>
-					<textarea class="form-control" rows="10" name="d" required="" readonly="">{{$siswa->alamat}}</textarea>
+					<textarea class="form-control" rows="10" name="d" readonly="">{{$siswa->alamat}}</textarea>
 				</div>
 			</form>
 		</div>
